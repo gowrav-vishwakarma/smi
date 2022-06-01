b527583f9657e7a6af8c90575dc50809d2de98db
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2022-06-01 12:41:53 +0530

    added server-1 as another best practice setup

--- server-ts/dist/controllers/userController.js
+++ server-ts/dist/controllers/userController.js
@@ -9,7 +9,10 @@ class UserController {
     }
     create_user(req, res) {
         // this check whether all the filds were send through the erquest or not
-        if (req.body.name && req.body.name.first_name && req.body.name.middle_name && req.body.name.last_name &&
+        if (req.body.name &&
+            req.body.name.first_name &&
+            req.body.name.middle_name &&
+            req.body.name.last_name &&
             req.body.email &&
             req.body.phone_number &&
             req.body.gender) {
@@ -17,29 +20,31 @@ class UserController {
                 name: {
                     first_name: req.body.name.first_name,
                     middle_name: req.body.name.middle_name,
-                    last_name: req.body.name.last_name
+                    last_name: req.body.name.last_name,
                 },
                 email: req.body.email,
                 phone_number: req.body.phone_number,
                 gender: req.body.gender,
-                modification_notes: [{
+                modification_notes: [
+                    {
                         modified_on: new Date(Date.now()),
                         modified_by: null,
-                        modification_note: 'New user created'
-                    }]
+                        modification_note: "New user created",
+                    },
+                ],
             };
             this.user_service.createUser(user_params, (err, user_data) => {
                 if (err) {
-                    (0, service_1.mongoError)(err, res);
+                    service_1.mongoError(err, res);
                 }
                 else {
-                    (0, service_1.successResponse)('create user successfull', user_data, res);
+                    service_1.successResponse("create user successfull", user_data, res);
                 }
             });
         }
         else {
             // error response if some fields are missing in request body
-            (0, service_1.insufficientParameters)(res);
+            service_1.insufficientParameters(res);
         }
     }
     get_user(req, res) {
@@ -47,81 +52,99 @@ class UserController {
             const user_filter = { _id: req.params.id };
             this.user_service.filterUser(user_filter, (err, user_data) => {
                 if (err) {
-                    (0, service_1.mongoError)(err, res);
+                    service_1.mongoError(err, res);
                 }
                 else {
-                    (0, service_1.successResponse)('get user successfull', user_data, res);
+                    service_1.successResponse("get user successfull", user_data, res);
                 }
             });
         }
         else {
-            (0, service_1.insufficientParameters)(res);
+            service_1.insufficientParameters(res);
         }
     }
     update_user(req, res) {
-        if (req.params.id &&
-            req.body.name || req.body.name.first_name || req.body.name.middle_name || req.body.name.last_name ||
+        if ((req.params.id && req.body.name) ||
+            req.body.name.first_name ||
+            req.body.name.middle_name ||
+            req.body.name.last_name ||
             req.body.email ||
             req.body.phone_number ||
             req.body.gender) {
             const user_filter = { _id: req.params.id };
             this.user_service.filterUser(user_filter, (err, user_data) => {
                 if (err) {
-                    (0, service_1.mongoError)(err, res);
+                    service_1.mongoError(err, res);
                 }
                 else if (user_data) {
                     user_data.modification_notes.push({
                         modified_on: new Date(Date.now()),
                         modified_by: null,
-                        modification_note: 'User data updated'
+                        modification_note: "User data updated",
                     });
                     const user_params = {
                         _id: req.params.id,
-                        name: req.body.name ? {
-                            first_name: req.body.name.first_name ? req.body.name.first_name : user_data.name.first_name,
-                            middle_name: req.body.name.first_name ? req.body.name.middle_name : user_data.name.middle_name,
-                            last_name: req.body.name.first_name ? req.body.name.last_name : user_data.name.last_name
-                        } : user_data.name,
-                        email: req.body.email ? req.body.email : user_data.email,
-                        phone_number: req.body.phone_number ? req.body.phone_number : user_data.phone_number,
-                        gender: req.body.gender ? req.body.gender : user_data.gender,
-                        is_deleted: req.body.is_deleted ? req.body.is_deleted : user_data.is_deleted,
-                        modification_notes: user_data.modification_notes
+                        name: req.body.name
+                            ? {
+                                first_name: req.body.name.first_name
+                                    ? req.body.name.first_name
+                                    : user_data.name.first_name,
+                                middle_name: req.body.name.first_name
+                                    ? req.body.name.middle_name
+                                    : user_data.name.middle_name,
+                                last_name: req.body.name.first_name
+                                    ? req.body.name.last_name
+                                    : user_data.name.last_name,
+                            }
+                            : user_data.name,
+                        email: req.body.email
+                            ? req.body.email
+                            : user_data.email,
+                        phone_number: req.body.phone_number
+                            ? req.body.phone_number
+                            : user_data.phone_number,
+                        gender: req.body.gender
+                            ? req.body.gender
+                            : user_data.gender,
+                        is_deleted: req.body.is_deleted
+                            ? req.body.is_deleted
+                            : user_data.is_deleted,
+                        modification_notes: user_data.modification_notes,
                     };
                     this.user_service.updateUser(user_params, (err) => {
                         if (err) {
-                            (0, service_1.mongoError)(err, res);
+                            service_1.mongoError(err, res);
                         }
                         else {
-                            (0, service_1.successResponse)('update user successfull', null, res);
+                            service_1.successResponse("update user successfull", null, res);
                         }
                     });
                 }
                 else {
-                    (0, service_1.failureResponse)('invalid user', null, res);
+                    service_1.failureResponse("invalid user", null, res);
                 }
             });
         }
         else {
-            (0, service_1.insufficientParameters)(res);
+            service_1.insufficientParameters(res);
         }
     }
     delete_user(req, res) {
         if (req.params.id) {
             this.user_service.deleteUser(req.params.id, (err, delete_details) => {
                 if (err) {
-                    (0, service_1.mongoError)(err, res);
+                    service_1.mongoError(err, res);
                 }
                 else if (delete_details.deletedCount !== 0) {
-                    (0, service_1.successResponse)('delete user successfull', null, res);
+                    service_1.successResponse("delete user successfull", null, res);
                 }
                 else {
-                    (0, service_1.failureResponse)('invalid user', null, res);
+                    service_1.failureResponse("invalid user", null, res);
                 }
             });
         }
         else {
-            (0, service_1.insufficientParameters)(res);
+            service_1.insufficientParameters(res);
         }
     }
 }
