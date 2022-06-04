5dda526ab2be9e2ac4cf4f74624dc729d25a6b92
Malay Srivastava <malaypreet2013@gmail.com>
2022-06-04 12:18:32 +0530

    passport

--- server/app.js
+++ server/app.js
@@ -20,11 +20,23 @@ const cors = require('cors');
 app.use(cors({
     origin: '*'
 }));
+const session = require('express-session');  // session middleware
+const passport = require('passport');  // authentication
+const connectEnsureLogin = require('connect-ensure-login'); //authorization
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(session({
+    secret: 'r8q,+&1LM3)CD*zAGpx1xm{NeQhc;#',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60 * 60 * 1000 } // 1 hour
+  }));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
