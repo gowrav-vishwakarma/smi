58629568466b52d35784df773b2aeef3fb3118e0
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2022-06-05 18:25:42 +0530

    Working on NavBars

---
+++ client/src/components/UI/BottomNavigation.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-bottom-navigation class="justify-space-between px-2">
+        <v-btn to="/" value="recent">
+            <span>Home</span>
+            <v-icon>mdi-home</v-icon>
+        </v-btn>
+
+        <v-btn to="/login" value="nearby">
+            <span>Login</span>
+
+            <v-icon>mdi-login</v-icon>
+        </v-btn>
+    </v-bottom-navigation>
+</template>
---
+++ client/src/components/UI/NavbarLoggedIn.vue
@@ -0,0 +1,32 @@
+<template>
+    <nav class="mb-2 px-0">
+        <v-app-bar dense app class="">
+            <v-toolbar-title class="">
+                <v-btn plain :ripple="false" class="" to="/">
+                    <v-img src="@/assets/logo.png" class="mr-2"></v-img>
+                    Solve My Issue
+                </v-btn>
+            </v-toolbar-title>
+
+            <v-spacer></v-spacer>
+            <v-divider vertical></v-divider>
+            <v-toolbar-items>
+                <v-btn plain class="" to="/ask-question"> Ask Question </v-btn>
+            </v-toolbar-items>
+            <v-toolbar-items>
+                <v-btn plain class="" @click="logout"> Logout </v-btn>
+            </v-toolbar-items>
+        </v-app-bar>
+        <!-- End of app toolbar -->
+    </nav>
+</template>
+
+<script>
+export default {
+    methods: {
+        logout() {
+            this.$store.commit("logout");
+        },
+    },
+};
+</script>
