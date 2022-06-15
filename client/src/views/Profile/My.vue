bd74fb80225bf510088656b115589f7cdacd681b
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2022-06-15 10:40:53 +0530

    profile page comented on

---
+++ client/src/views/Profile/My.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-container> Profile </v-container>
+    <!-- 
+        create components/Profile/My/foo-bars components to place here and to be
+        reused in the My page.
+     -->
+</template>
+
+<script>
+export default {
+    name: "ProfilePage",
+    components: {},
+};
+</script>
