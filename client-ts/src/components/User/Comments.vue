4df9825f0a27e2a348d4803767305f26dbf894d0
Malay Srivastava <malaypreet2013@gmail.com>
2022-07-27 20:36:47 +0530

    question-commit

--- client-ts/src/components/User/Comments.vue
+++ client-ts/src/components/User/Comments.vue
@@ -1,20 +1,32 @@
 <template lang="pug">
-    .caption My Comments
+    div(style="width: 100%") 
+      v-card.d-flex.flex-column.mb-2.pa-2(v-for="(comment,i) in comments" :key="i")
+        h4.blue--text(@click="gotoDetails(comment.questionId)" style="cursor:pointer") Go to Question 
+        p.mt-3
+        .caption {{ shortdetail(comment.comment) }}
+        small.mt-2
+        .caption.ml-auto.grey--text.lighten-4 asked {{ humanized_time_span(comment.createdAt) }}
+    
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Mixins } from "vue-property-decorator";
+import { General } from "@/mixins/general";
 import userApi from "@/services/user.api";
+import S from "string";
 
 @Component({
   name: "UserComments",
 })
-export default class UserCommentsComponent extends Vue {
+export default class UserCommentsComponent extends Mixins(General){
     comments: any[] = [];
 
   async mounted() {
     this.comments = await userApi.getMyComments();
     console.log(this.comments);
+  }
+  shortdetail(detail:string) {
+    return S(detail).stripTags().truncate(100).s;
   }
 }
 </script>
--- client-ts/src/components/User/Questions.vue
+++ client-ts/src/components/User/Questions.vue
@@ -1,14 +1,20 @@
 <template lang="pug">
-    .caption My Questions
+div(style="width: 100%")
+  question-single(v-for="(question, i) in questions" :question="question" :key="questions[i]._id")
 </template>
+
 
 <script lang="ts">
 import QuestionListResponseDTO from "@/dto/response/question-list-response.dto";
 import userApi from "@/services/user.api";
 import { Component, Vue } from "vue-property-decorator";
+import QuestionSingle from "@/components/Question/Single.vue";
 
 @Component({
   name: "UserQuestions",
+  components: {
+    QuestionSingle,
+  },
 })
 export default class UserQuestionsComponent extends Vue {
   questions: QuestionListResponseDTO[] = [];
